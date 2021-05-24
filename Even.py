7e3828dda2704346bb095be3de769896351083da
NAVYA CHENNUPATI <84490199+Navyach1@users.noreply.github.com>
2021-05-24 12:25:24 +0530

    Add files via upload

---
+++ Even.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+
+if a % 2 == 0:
+			print("a is even")
+else:
+	print("a is odd")
+if b % 2 == 0:
+			print("b is even")
+else:
+	print("b is odd")
+
+'''output:
+		3
+		6
+		a is odd
+		b is even'''
