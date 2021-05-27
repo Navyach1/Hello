1b12e064b8c8b2f0d785a0daf7c879c8e836e336
NAVYA CHENNUPATI <84490199+Navyach1@users.noreply.github.com>
2021-05-27 10:17:21 +0530

    Update discount.py

--- discount.py
+++ discount.py
@@ -17,7 +17,7 @@ elif n in range(3000,5000):
 	print(Discount)
 	AmountToBePaid = n - Discount
 	print(f"TotalAmount is {AmountToBePaid}")
-elif n > 5000:
+elif n >= 5000:
 	print("Discount is 40%")
 	Discount = (40/100) * n
 	print(Discount)
