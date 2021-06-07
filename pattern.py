24dc479181fb7323c22fe159d4cde0711273d289
NAVYA CHENNUPATI <84490199+Navyach1@users.noreply.github.com>
2021-06-07 12:29:37 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -3,12 +3,3 @@ for i in range(0,n):
 	for j in range(0,i):
 		print(j, end = " ")
 	print(i)
-
-
-
-
-'''n = int(input("Enter n value: "))
-for i in range(1,n+1):
-	for j in range(n+1, n+6):
-		print(i, end = " ")
-	print()'''
